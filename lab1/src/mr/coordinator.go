a1c7d648228f2d9ed1f1975f86d8272d9d9137b3
C K <qiukan97@gmail.com>
2023-12-03 21:54:36 -0800

    Add Done method to check if entire process has finished

--- lab1/src/mr/coordinator.go
+++ lab1/src/mr/coordinator.go
@@ -54,12 +54,7 @@ func (c *Coordinator) server() {
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-	ret = true
-	return ret
+	return c.isSuccess
 }
 
 // create a Coordinator.
